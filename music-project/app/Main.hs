1fafc6b12466dbc5a10472753e20423e755c7db8
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2022-11-29 19:04:49 +0000

    Trying to fetch \n has a bit of errors

--- music-project/app/Main.hs
+++ music-project/app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
 import Lib
+import Fetch
+import Parse
 
 main :: IO ()
-main = someFunc
+main = fetchFunction
+
+fetchFunction = do
+    let url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
+    print "Downloading..."
+    json <- download url
+    print "Parsing..."
+    case (parseRecords json) of
+        Left err -> print err
+        Right recs -> do
+            print "Done Parsing"
+            main
